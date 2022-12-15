a196868b5eb4b5456bec91b6dc757626582f002e
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-12-14 21:16:58 -0300

    Update jwtAuth.js

---
+++ backend/api/middlewares/jwtAuth.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+
+const jwtAuth = (req, res, next) =>{
+    let token = req.headers['x-access-token']
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) =>{
+        if(err){
+            return res.status(401).end()
+        }
+        req.userID = decoded.id
+        next()
+    })
+}
+
+module.exports = jwtAuth
