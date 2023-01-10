ba4c8cf6ecd2046908e8f4415e75454f20f8ee25
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-01-10 19:37:15 -0300

    Update database.js

--- backend/api/config/database.js
+++ backend/api/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
-  port: 3306
+  port: process.env.DB_PORT
 });
 
 
