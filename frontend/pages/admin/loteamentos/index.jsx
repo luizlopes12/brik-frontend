f31e6051f9ea6ad2ee3f6d6f038aa82589ce30b3
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-01-18 21:56:01 -0300

    continuing loteamento's page

--- frontend/pages/admin/loteamentos/index.jsx
+++ frontend/pages/admin/loteamentos/index.jsx
@@ -1,62 +1,97 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './style.module.scss'
 import SearchInput from '../../../components/SearchInput'
 import HeadingText from '../../../components/HeadingText'
 
-export async function getStaticProps(){
+export async function getStaticProps() {
     const divisionsData = await fetch('http://localhost:8080/divisions/list').then(res => res.json())
-    return{
+    return {
         props: { divisionsData }
     }
 }
 
 const Loteamentos = ({ divisionsData }) => {
-    console.log(divisionsData)
-  const Availabilities = [{name:'Disponível', value: 'avaible'}, {name:'Indisponível', value: 'unavaible'},{name:'Reservado', value: 'reserved'}]
-  const [selectValues, setSelectValues] = useState({
-    division: 'all',
-    availability: 'avaible'
-  })
-  useEffect(() =>{
-      console.log(selectValues)
-  },[selectValues])
-  const handleSelectItem = (e) =>{
-      setSelectValues(previousState =>({...previousState, [e.target.name]: e.target.value}))
-  }
+    const Availabilities = [{ name: 'Disponível', value: 'avaible' }, { name: 'Indisponível', value: 'unavaible' }, { name: 'Reservado', value: 'reserved' }]
+    const [selectValues, setSelectValues] = useState({
+        division: 'all',
+        availability: 'avaible'
+    })
+    const [lotsData, setLotsData] = useState(divisionsData.map((division) => division.lotes).flat())
+    console.log(lotsData)
+    const handleSelectItem = (e) => {
+        setSelectValues(previousState => ({ ...previousState, [e.target.name]: e.target.value }))
+    }
 
-  return (
-    <section className={style.loteamentosContainer}>
-        <div className={style.heading}>
-            <HeadingText>Lotes e Loteamentos</HeadingText>
-            <SearchInput/>
-        </div>
-        <div className={style.topActions}>
-            <div className={style.lotFilters}>
-              <div className={style.dropdownsContainer}>
-              <select onChange={handleSelectItem} value={selectValues.division} name='division' className={style.dropdownMenu}> 
-                  {divisionsData.map((item, index) =>( 
-                      <>
-                      <option selected value='all' >Todos</option>
-                      <option key={index} value={item.id}>{item.name}</option>
-                    </>
-                  ))}
-              </select>
-              <select onChange={handleSelectItem} value={selectValues.availability} name='availability' className={style.dropdownMenu}> 
-                  {Availabilities.map((item, index) =>( 
-                      <option key={index} value={item.value}>{item.name}</option>
-                  ))}
-              </select>
-              </div>
+    return (
+        <section className={style.loteamentosContainer}>
+            <div className={style.heading}>
+                <HeadingText>Lotes e Loteamentos</HeadingText>
+                <SearchInput />
+            </div>
+            <div className={style.topActions}>
+                <div className={style.lotFilters}>
+                    <div className={style.dropdownsContainer}>
+                        <select onChange={handleSelectItem} value={selectValues.division} name='division' className={style.dropdownMenu}>
+                            <option value='all'>Todos</option>
+                            {divisionsData.map((item, index) => (
+                                <>
+                                    <option key={index} value={item.id}>{item.name}</option>
+                                </>
+                            ))}
+                        </select>
+                        <select onChange={handleSelectItem} value={selectValues.availability} name='availability' className={style.dropdownMenu}>
+                            {Availabilities.map((item, index) => (
+                                <option key={index} value={item.value}>{item.name}</option>
+                            ))}
+                        </select>
+                    </div>
 
-              <SearchInput/>
+                    <SearchInput />
+                </div>
+                <div className={style.lotHandleActions}>
+                    <button className={style.btnTaxes} onClick={() => alert('Editar juros')}><img src='/images/taxesIcon.svg' />Editar juros</button>
+                    <button className={style.btnLot} onClick={() => alert('Cadastrar lote')}><img src='/images/plusIcon.svg' />Cadastrar lote</button>
+                </div>
             </div>
-            <div className={style.lotHandleActions}>
-                <button className={style.btnTaxes}><img  src='/images/taxesIcon.svg'/>Editar juros</button>
-                <button className={style.btnLot}><img  src='/images/plusIcon.svg'/>Cadastrar lote</button>
+            <div className={style.listsContainer}>
+                <ul className={style.lotsList}>
+                    {lotsData.map((lot) => (
+                        <li className={style.lotsListItem}>
+                            <div className={style.lotImage}>
+                                <img src={lot.loteImages[0]?.imageUrl} alt="Imagem do lote" />
+                            </div>
+                            <div className={style.lotInfos}>
+                                <div className={style.lotSpecs}>
+                                    <span className={style.lotName}>{lot.name}</span>
+                                    <span className={style.divName}>{divisionsData.find(division => division.id == lot.idLoteamento).name.substring(0, 20) + '...'}</span>
+                                    <div className={style.location}>
+                                        <span>
+                                        <img src="/images/locationIcon.svg" />
+                                            {lot.location.substring(0, 20) + '...'}
+                                        </span>
+                                        <span>
+                                        <img src="/images/metricsIcon.svg" />
+
+                                            {lot.metrics}
+                                            <sup>2</sup>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className={style.lotPrice}>
+                                    <p>R$ {lot.finalPrice}</p>
+                                    <p>R$ {lot.basePrice}</p>
+                                </div>
+                                <button className={style.lotOptionsBtn} onClick={() => alert('Options')}>...</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <ul className={style.divisionssList}>
+                    
+                </ul>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Loteamentos
