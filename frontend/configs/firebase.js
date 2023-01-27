573f596dbc108805e4c90bc925164b24e3dbf124
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-01-26 22:38:26 -0300

    Enviroment vars

--- frontend/configs/firebase.js
+++ frontend/configs/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEzv1tcawyq2irN1Iv2Zeldt_XCSxlvNo",
-  authDomain: "brik-files.firebaseapp.com",
-  projectId: "brik-files",
-  storageBucket: "brik-files.appspot.com",
-  messagingSenderId: "1091977981291",
-  appId: "1:1091977981291:web:0af7a176271d764ea1702e"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
