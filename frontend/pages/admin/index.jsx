04375edc08b4eceab9d593fe414f0a95aab2e6fc
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-03-05 09:09:23 -0300

    Update index.jsx

--- frontend/pages/admin/index.jsx
+++ frontend/pages/admin/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
 import style from './style.module.scss'
 import HeadingText from '../../components/HeadingText'
 
 const Admin = () => {
-  const router = useRouter()
   return (
     <div className={style.overviewContainer}>
     <div className={style.heading}>
